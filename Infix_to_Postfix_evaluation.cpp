ab503c297b5289c8b55b78ce96838f03579a6679
Muhammad Umar Asad <umarasad20@gmail.com>
2021-11-14 23:15:59 +0500

    added comments in the program and a under development feature

--- Infix_to_Postfix_evaluation.cpp
+++ Infix_to_Postfix_evaluation.cpp
@@ -174,27 +174,23 @@ bool correctPrecedence(char top, char o)
 //in development feature to check all kinds of brackets
 void isSqClBracket(char bracket)
 {
-    if(bracket == ']')
+    if (bracket == ']')
     {
-
     }
 }
 
 void isFlClBracket(char bracket)
 {
-    if(bracket == '}')
+    if (bracket == '}')
     {
-
     }
 }
 void isCClBracket(char bracket)
 {
-    if(bracket == ')')
+    if (bracket == ')')
     {
-
     }
 }
-
 
 //Infix to Postfix Class
 class InfixToPostfix
@@ -209,15 +205,18 @@ class InfixToPostfix
     string postFixTerm = "", temp = "";
     char temp_char = ' ', temp_char_1 = ' ';
 
-//public functions
+    //public functions
 public:
     string InfixToPostfixConversation(string infixExpression)
     {
 
+        //loop to read all the characters
         for (int i = 0; i <= infixExpression.size(); i++)
         {
+            //till where it is a digit it will be stored in a variable
             if (isdigit(infixExpression[i]))
             {
+                //incrementing
                 temp += infixExpression[i];
             }
             else
@@ -228,17 +227,26 @@ public:
                 postFixTerm += " ";
                 temp = "";
 
+                //getting the top character of the stack and using it
                 operatorStack.peak(&temp_char);
+                //if string character is a operator and its precedence is correct
+                //push it into the stack
                 if (temp_char == ' ' || correctPrecedence(temp_char, infixExpression[i]))
                 {
                     operatorStack.Push(infixExpression[i]);
                 }
 
+                //if there is a closing bracket in the string expression
+                //pop all the operators one by one till closing bracket comes
                 else if (isClosingBracket(infixExpression[i]))
                 {
+                    //if the character pooped is opening bracket and there was no operator in between them
+                    //then loop does not run
+                    //else loop pops operators one by one and add them in stack
                     temp_char_1 = operatorStack.Pop();
                     while (!isOpeningBracket(temp_char_1))
                     {
+                        //incrementing the operator in the output string from stack
                         postFixTerm += temp_char_1;
                         postFixTerm += ' ';
                         temp_char_1 = operatorStack.Pop();
@@ -247,6 +255,8 @@ public:
 
                 else
                 {
+                    //when the operator precdence is such that operator of
+                    //equal precedence is in input expression and stack top
                     if (!correctPrecedence(temp_char, infixExpression[i]) || infixExpression[i] == temp_char)
                     {
                         postFixTerm += operatorStack.Pop();
@@ -254,8 +264,12 @@ public:
                         operatorStack.Push(infixExpression[i]);
                     }
 
+                    //when the operator precdence is such that operator of
+                    //greater precedence is in input expression and stack top
                     if (!correctPrecedence(temp_char, infixExpression[i]))
                     {
+                        //while the correct precedence doesnot come it pops the elements form the stack
+                        // and pushes them in the string output
                         while (!operatorStack.isEmpty() && !correctPrecedence(temp_char, infixExpression[i]))
                         {
                             postFixTerm += operatorStack.Pop();
@@ -267,50 +281,76 @@ public:
                 }
             }
         }
+        //returning the postfix term
         return postFixTerm;
     }
 
+    //to calculate the expression
     double PostfixEvaluation(string postfixExpression)
     {
-        
+
+        //loop till it reads all the characters
         for (int i = 0; i < postfixExpression.size(); i++)
         {
+            //if string character is a digit
             if (isdigit(postfixExpression[i]))
             {
                 number += postfixExpression[i];
             }
+
+            //if string character is a space
             else if (postfixExpression[i] == ' ' && number != "")
-            {
-                    outputStack.Push(stod(number));
-                    number = "";
+            {   
                 
+                //stod is used to convert the string into a double 
+                //then pushing the converted number into the output stack
+                outputStack.Push(stod(number));
+                number = "";
             }
 
+            //if string character is an operator
             else if (isOperator(postfixExpression[i]))
             {
+
+                //poping the most two above elemets from the stack
                 above = outputStack.Pop();
                 below = outputStack.Pop();
 
+                /*
+                evaluating different conditions of the operators and evaluating
+                the answers and pushing it again in the stack to continue the operations
+                */
+
+                //for sum
                 if (postfixExpression[i] == '+')
                 {
-                    result = below + above;
+                    result = above + below;
                 }
+
+                // for subtraction
                 else if (postfixExpression[i] == '-')
                 {
                     result = below - above;
                 }
+
+                //for multiplication
                 else if (postfixExpression[i] == '*')
                 {
-                    result = below * above;
+                    result = above * below;
                 }
+
+                //for divison
                 else if (postfixExpression[i] == '/')
                 {
                     result = below / above;
                 }
+
+                //for power
                 else if (postfixExpression[i] == '^')
                 {
                     result = pow(below, above);
                 }
+                //pushing the result in the output stack
                 outputStack.Push(result);
             }
         }
@@ -329,19 +369,30 @@ T remove_if(T beg, T end, P pred)
     return dest;
 }
 
+
 int main()
 {
-    InfixToPostfix p;
-    string infix;
-    string postfix;
+
+    InfixToPostfix infixToPostfix;
+    string infix , postfix;
     double result;
+
+    //getting the infix expression from the user
     cout << "Enter the infix expression:  ";
     getline(cin, infix);
+
+    //removing all the whitespaces form the infix input
     infix.erase(remove_if(infix.begin(), infix.end(), ::isspace), infix.end());
+
+    //printing the infix expression
     cout << "The infix without spaces is :  " << infix << endl;
-    postfix = p.InfixToPostfixConversation(infix);
+
+    //printing the postfix expression
+    postfix = infixToPostfix.InfixToPostfixConversation(infix);
     cout << "The PostFix Expression is :  " << postfix << endl;
-    result = p.PostfixEvaluation(postfix);
+
+    //printing the answer
+    result = infixToPostfix.PostfixEvaluation(postfix);
     cout << "The evaluated answer is :" << result << endl;
 
     return 0;
